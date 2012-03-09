946a8bf2cdb9699d1aab8323571ff692d46137f1
Thomas Pun <thomas@nowbox.com>
2012-03-09 00:34:11 -0800

    not working with overlapping sequence yet

--- luhn_filter.rb
+++ luhn_filter.rb
@@ -1,15 +1,49 @@
+class String
+  def substrings min_length, only
+    out = []
+    if self.length >= min_length
+      self.length.times do |n|
+        break if self.length - n < min_length
+
+        substring = ""
+        substring_count = 0 # actual count disguarding ignored
+        self[n..-1].split("").each do |char|
+          break if substring_count == min_length
+
+          substring << char
+          substring_count += 1 if only.include? char
+        end
+        out << substring if substring_count==min_length
+      end
+    end
+    out
+  end
+end
+
 class LuhnFilter
   def initialize unfiltered
-    @unfiltered = unfiltered.chomp
+    @unfiltered = unfiltered
   end
 
   def filtered
-    return masked @unfiltered if passed?
+    only = (0..9).map &:to_s
+    possible_substrings((14..16), only).each do |unsanitized|
+      return masked unsanitized if passed? unsanitized
+    end
     @unfiltered
   end
 
-  def passed?
-    digits = remove_non_digits
+  def possible_substrings range, ignored
+    out = []
+    range.reverse_each do |n|
+      out += @unfiltered.substrings n, ignored
+    end
+    out
+  end
+
+  def passed? unsanitized
+    digits = remove_non_digits unsanitized
+
     length = digits.length
     return false unless (14..16).cover? length
 
@@ -27,12 +61,13 @@ class LuhnFilter
     sum % 10 == 0
   end
 
-  def masked string
-    string.gsub /[0123456789]/, 'X'
+  def masked matched
+    replacement = matched.gsub /[0-9]/, 'X'
+    @unfiltered.gsub matched, replacement
   end
 
-  def remove_non_digits
-    @unfiltered.gsub /[A-Z+a-z+\-+\ ]/, ''
+  def remove_non_digits unsanitized
+    unsanitized.gsub /[A-Z+a-z+\-+\ ]/, ''
   end
 end
 

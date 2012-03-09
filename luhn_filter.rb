003c1751048dc11094eab8e02bb41b90d742043f
Thomas Pun <thomas@nowbox.com>
2012-03-09 01:26:23 -0800

    it passes overlapping tests but it's really really slow

--- luhn_filter.rb
+++ luhn_filter.rb
@@ -1,44 +1,56 @@
+require 'set'
+
 class String
-  def substrings min_length, only
-    out = []
-    if self.length >= min_length
-      self.length.times do |n|
-        break if self.length - n < min_length
+  # first `n` appearance of characters in `only`
+  def first n, only
+    return "" if self.length < n
 
-        substring = ""
-        substring_count = 0 # actual count disguarding ignored
-        self[n..-1].split("").each do |char|
-          break if substring_count == min_length
-
-          substring << char
-          substring_count += 1 if only.include? char
-        end
-        out << substring if substring_count==min_length
+    substring = ""
+    count = 0
+    self.length.times do |i|
+      (i..self.length-1).each do |i|
+        char = self[i]
+        break if count == n
+        substring << char
+        count += 1 if only.include? char
       end
     end
-    out
+
+    if count == n
+      substring
+    else
+      "" # since we didn't get the requested length
+    end
+  end
+
+  def mask_digits! start, count, replacement
+    self[start..start+count-1] = replacement * count
   end
 end
 
 class LuhnFilter
+  MinLength = 14
+  MaxLength = 16
+
   def initialize unfiltered
-    @unfiltered = unfiltered
+    @unfiltered = unfiltered.chomp
+    @filtered = unfiltered.clone
   end
 
   def filtered
-    only = (0..9).map &:to_s
-    possible_substrings((14..16), only).each do |unsanitized|
-      return masked unsanitized if passed? unsanitized
-    end
-    @unfiltered
-  end
+    only = Set.new (0..9).map &:to_s
+    @unfiltered.length.times do |n|
+      break if @unfiltered.length - n < MinLength
 
-  def possible_substrings range, ignored
-    out = []
-    range.reverse_each do |n|
-      out += @unfiltered.substrings n, ignored
+      (MinLength..MaxLength).reverse_each do |length|
+        unsanitized = @unfiltered[n..-1].first length, only
+        if passed? unsanitized
+          @filtered.mask_digits! n, unsanitized.length, 'X'
+          break # since we don't need to test for shorter code
+        end
+      end
     end
-    out
+    @filtered
   end
 
   def passed? unsanitized
@@ -61,11 +73,6 @@ class LuhnFilter
     sum % 10 == 0
   end
 
-  def masked matched
-    replacement = matched.gsub /[0-9]/, 'X'
-    @unfiltered.gsub matched, replacement
-  end
-
   def remove_non_digits unsanitized
     unsanitized.gsub /[A-Z+a-z+\-+\ ]/, ''
   end
@@ -74,6 +81,7 @@ end
 
 # # Read from stdin
 $stdin.each_line do |line|
+$stderr.puts "test: #{line}"
   luhn = LuhnFilter.new line
   $stdout.puts luhn.filtered
 end
